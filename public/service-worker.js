6a5fea7c339ee36c6d0cc16720e4e2a900f8a2cb
Zmoore <zmoore371@gmail.com>
2021-12-15 12:11:30 -0500

    Added option into server.js to remove deprication warning for MongoDB, Started building items to cache on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,21 @@
 console.log("Hey Im Connected")
+
+
+const CACHE_NAME = "static-cache-v2"
+const DATA_CACHE_NAME = "data-cache-v1"
+
+const FILES_TO_CACHE = [
+    "/",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css"
+]
+
+// self.addEventListener("install", function (e) {
+//     e.waitUntil(
+//         caches.open(DATA_CACHE_NAME).then((cache) => cache.add())
+//     )
+// })
