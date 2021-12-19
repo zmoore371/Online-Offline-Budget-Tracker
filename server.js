b79f993589df75245fa52b773890530015b684d0
Zmoore <zmoore371@gmail.com>
2021-12-18 23:21:51 -0500

    changes 4

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());\
+app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
@@ -24,6 +24,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
