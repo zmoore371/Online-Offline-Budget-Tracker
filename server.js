fd9db2b8fc67a358cf5cc58c24f9f7557dac4df4
Zmoore <zmoore371@gmail.com>
2021-12-18 23:09:44 -0500

    changes 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -11,8 +11,7 @@ app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+app.use(express.json());\
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
